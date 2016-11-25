e80f1d6373c0e3be7f9039ca5963f25b983b0077
Sebastian Grunow <sebastian@grunow-it.de>
2016-11-25 15:59:28 +0100

    Implemented D3 Career and Hero Profile endpoints.

---
+++ BattleNetApi.Client/Endpoints/D3/Profile.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using System.Web;
+using BattleNetApi.Client.Models.D3;
+
+namespace BattleNetApi.Client
+{
+    public partial class D3ApiClient
+    {
+        private static string GetCareerProfileUrl(string battleTag)
+        {
+            return $"/d3/profile/{HttpUtility.UrlEncode(battleTag)}/";
+        }
+
+        private static string GetHeroProfileUrl(string battleTag, int heroId)
+        {
+            return GetCareerProfileUrl(battleTag) + $"hero/{heroId}";
+        }
+
+        #region Career Profile
+        public CareerProfile GetCareerProfile(string battleTag)
+        {
+            return GetApiResponse(ForgeApiRequest<CareerProfile>(GetCareerProfileUrl(battleTag)));
+        }
+
+        public async Task<CareerProfile> GetCareerProfileAsync(string battleTag)
+        {
+            return await GetApiResponseAsync(ForgeApiRequest<CareerProfile>(GetCareerProfileUrl(battleTag)));
+        }
+        #endregion
+
+        #region Hero Profile
+        public Hero GetHeroProfile(string battleTag, int heroId)
+        {
+            return GetApiResponse(ForgeApiRequest<Hero>(GetHeroProfileUrl(battleTag, heroId)));
+        }
+
+        public async Task<Hero> GetHeroProfileAsync(string battleTag, int heroId)
+        {
+            return await GetApiResponseAsync(ForgeApiRequest<Hero>(GetHeroProfileUrl(battleTag, heroId)));
+        }
+        #endregion
+    }
+}
